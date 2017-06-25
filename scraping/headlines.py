14fb0cfc8a74e151ecf97950567b8a382247311a
rubygeek <nola@rubygeek.com>
2017-06-25 14:58:39 -0500

    updated with our scraping example

--- scraping/headlines.py
+++ scraping/headlines.py
@@ -7,7 +7,8 @@ html = response.read()
 soup = BeautifulSoup(html, "html.parser")
 headlines = soup.find_all("h1", class_="entry-title")
 
-for headline in headlines:
-    print headline.string
+headlines_set = set(headlines)
 
+for headline in headlines_set:
+    print headline.string
     
